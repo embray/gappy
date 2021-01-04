62f8796e8f318236ebc21ccfaa39d7189a0213cc
E. Madison Bray <erik.bray@lri.fr>
2021-01-04 16:01:20 +0100

    Initial commit with minimal project skeleton.

---
+++ gappy/_dev/__init__.py
@@ -0,0 +1,5 @@
+"""
+Utilities used only for build and development of gappy.
+
+This package is excluded from installations.
+"""
---
+++ setup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# stub setup.py for backwards compatibility
+from setuptools import setup
+
+setup(use_scm_version={'write_to': 'gappy/_version.py'})
