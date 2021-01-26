c4c444b6c0b9cf32227d9c47363f9609014f08f7
E. Madison Bray <erik.bray@lri.fr>
2021-01-26 17:46:01 +0100

    Really forcibly disable this warning at its source when in CI.
    pytest's filterwarnings option doesn't even catch it since gappy is
    imported while displaying the pytest report banner.

--- gappy/_dev/scm_version.py
+++ gappy/_dev/scm_version.py
@@ -1,6 +1,12 @@
+import os
 import os.path as pth
+import warnings
 try:
     from setuptools_scm import get_version
+    if 'CI' in os.environ:
+        # ignore warning about shallow git repositories when running
+        # CI builds where this is not so important I think
+        warnings.filterwarnings('ignore', '.*is shallow and may cause errors')
     version = get_version(root=pth.join('..', '..'), relative_to=__file__)
 except Exception:
     raise ImportError(
